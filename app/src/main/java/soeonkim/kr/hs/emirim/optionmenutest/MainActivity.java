5257d3bf53162fcbf11154ebc455ce9e67f6ae17
Kimsoeon <a1thdjs@naver.com>
2017-05-22 20:49:08 +0900

    #2 Call back method

--- app/src/main/java/soeonkim/kr/hs/emirim/optionmenutest/MainActivity.java
+++ app/src/main/java/soeonkim/kr/hs/emirim/optionmenutest/MainActivity.java
@@ -2,12 +2,24 @@ package soeonkim.kr.hs.emirim.optionmenutest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        MenuInflater mInflater = getMenuInflater();
+        mInflater.inflate(R.menu.menu1, menu); //매개변수의 menu
+        return true;
     }
+
 }
