c05028263e958e1ae1a742297151752b916e57e4
Kimsoeon <a1thdjs@naver.com>
2017-05-23 19:55:51 +0900

    #3 Option Menu  Event Handling

--- app/src/main/java/soeonkim/kr/hs/emirim/optionmenutest/MainActivity.java
+++ app/src/main/java/soeonkim/kr/hs/emirim/optionmenutest/MainActivity.java
@@ -1,18 +1,23 @@
 package soeonkim.kr.hs.emirim.optionmenutest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    LinearLayout linear;
+    Button but;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        linear = (LinearLayout)findViewById(R.id.linear);
+        but = (Button) findViewById(R.id.but_ok);
         }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
@@ -22,4 +27,29 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.item_red:
+                linear.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.item_green:
+                linear.setBackgroundColor(Color.GREEN);
+                return true;
+            case R.id.item_blue:
+                linear.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.item_but_rotate:
+                but.setRotation(but.getRotation()+45);
+                return true;
+            case R.id.item_but_size:
+                but.setScaleX(2); //가로 방향으로 2배
+                but.setScaleY(2);
+                return true;
+
+
+        }
+        return false;
+    }
 }
